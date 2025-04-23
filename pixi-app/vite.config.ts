851ec1dc8d7b69bbc7787ad99149e5d791c645f8
Vlasenko Oleksii <lesha.ku.vlasenko@gmail.com>
2025-04-23 19:22:12 +0300

    Finished test app

---
+++ pixi-app/vite.config.ts
@@ -0,0 +1,6 @@
+// vite.config.ts
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/tic-tac-toe_PixiGame-/pixi-app/dist/', // <-- replace this with your actual GitHub repo name
+})
